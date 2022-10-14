3e98bc27e62cba991ff41d44484204c325987e2a
Tauriel1202 <lothlorienexplorer20@gmail.com>
2022-10-13 20:04:18 -0400

    swagger post working

--- routes/swagger.js
+++ routes/swagger.js
@@ -4,6 +4,6 @@ const swagUI = require("swagger-ui-express");
 const swagDocs = require("../swagger.json");
 
 routes.use('/api-docs', swagUI.serve);
-// routes.get("/api-docs", swagUI.setup(swagDocs));
+routes.get("/api-docs", swagUI.setup(swagDocs));
 
 module.exports = routes;
