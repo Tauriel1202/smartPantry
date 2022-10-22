1be62c6b165d7df98d1003a2589eca5fda480ca7
Theran Meadows <theranmeadows@Therans-MacBook-Air.local>
2022-10-21 22:20:38 -0700

    food jest testing

--- food.test.js
+++ food.test.js
@@ -1,30 +1,30 @@
-const food = require("../controllers/food");
+const food = require("./controllers/food");
 
 test("get all food", () => {
   expect(food.getAllFood).toBeDefined();
 } );
 
-// test("add one new food", () => {
-//   expect(food.addFood).toBeDefined();
-// });
+test("add one new food", () => {
+  expect(food.addFood).toBeDefined();
+});
 
-// test("get one - inCart", () => {
-//   expect(food.getFoodByInCart).toBeDefined();
-// } );
+test("get one - inCart", () => {
+  expect(food.getFoodByInCart).toBeDefined();
+} );
 
-// test("get one - cat", () => {
-//     expect(food.getFoodByCat).toBeDefined();
-// } );
+test("get one - cat", () => {
+    expect(food.getFoodByCat).toBeDefined();
+} );
 
-// test("get one - itemName", () => {
-//     expect(food.getFoodByItemName).toBeDefined();
-// } );
+test("get one - itemName", () => {
+    expect(food.getFoodByItemName).toBeDefined();
+} );
 
-// test("update one - id", () => {
-//     expect(food.updateFood).toBeDefined();
-// } );
+test("update one - id", () => {
+    expect(food.updateFood).toBeDefined();
+} );
 
-// test("delete one - id", () => {
-//     expect(food.deleteFood).toBeDefined();
-// } );
+test("delete one - id", () => {
+    expect(food.deleteFood).toBeDefined();
+} );
 
