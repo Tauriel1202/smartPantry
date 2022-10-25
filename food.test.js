2878d0aed4301ea36bd7267ccf8c2697e734f6e4
Theran Meadows <theranmeadows@Therans-MacBook-Air.local>
2022-10-25 11:36:03 -0700

    food jest testing

--- food.test.js
+++ food.test.js
@@ -1,3 +1,37 @@
+const {MongoClient} = require('mongodb');
+
+describe('insert', () => {
+    let connection;
+    let db;
+
+    beforeAll(async () => {
+        connection = await MongoClient.connect(global.__MONGO_URI__, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        db = await connection.db(global.__MONGO_DB_NAME__);
+    });
+
+    afterAll(async () => {
+        await connection.close();
+    });
+
+    it('should insert a doc into collection', async () => {
+        const food = db.collection('food');
+
+        const mockFood = {
+          _id: 'some-food-id',
+          cat: 'some-category',
+          itemName: 'some-name',
+          stock: 10,
+          inCart: false};
+        await food.insertOne(mockFood);
+
+        const insertedFood = await food.findOne({_id: 'some-food-id'});
+        expect(insertedFood).toEqual(mockFood);
+    });
+});
+
 const food = require("./controllers/food");
 
 test("get all food", () => {
